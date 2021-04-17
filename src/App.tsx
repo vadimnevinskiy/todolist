32d952cc9646548eb9aee45bc04e9b2cccad86c4
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-04-17 17:03:34 +0500

    Remove items and complited items

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,26 @@ function App() {
         setTodos(prev => [newTodo, ...prev]);
     }
 
+    const removeHandler = (id: number) => {
+        setTodos(prev => prev.filter(todo => todo.id !== id))
+    }
+
+    const toggleHandler = (id: number) => {
+        setTodos(prev => prev.map(todo => {
+
+            if(todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+            return todo
+        }))
+    }
+
     return (
         <div className="App">
             <Navbar />
             <div className="container">
                 <TodoForm onAdd={addHandler} />
-                <TodoList todos={todos} />
+                <TodoList todos={todos} onRemove={removeHandler} onToggle={toggleHandler} />
             </div>
         </div>
     );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,9 +3,12 @@ import {ITodo} from "../../interfaces";
 
 type TodoListProps = {
     todos: ITodo[]
+    onRemove: (id: number) => void
+    onToggle: (id: number) => void
+    // onToggle(id: number): void
 }
 
-export const TodoList: React.FunctionComponent<TodoListProps> = ({todos}) => {
+export const TodoList: React.FunctionComponent<TodoListProps> = ({todos, onRemove, onToggle}) => {
 
     if(todos.length === 0){
         return (
@@ -15,16 +18,33 @@ export const TodoList: React.FunctionComponent<TodoListProps> = ({todos}) => {
         )
     }
 
+    const removeHandler = (id: number) => {
+        onRemove(id)
+    }
+
+    const toggleHandler = (id: number) => {
+        onToggle(id)
+    }
+
     return (
         <ul className="">
             {
                 todos.map(item => {
+                    const classes = ['todo']
+
+                    if(item.completed){
+
+                        classes.push('completed')
+                    }
                     return (
-                        <li className="todo" key={item.id}>
+                        <li className={classes.join(' ')} key={item.id}>
                             <label>
-                                <input type="checkbox"/>
+                                <input type="checkbox" checked={item.completed} onChange={event=> toggleHandler(item.id)}/>
                                 <span>{item.title}</span>
-                                <i className="material-icons red-text">delete</i>
+                                <i
+                                    className="material-icons red-text"
+                                    onClick={event=> removeHandler(item.id)}
+                                >delete</i>
                             </label>
                         </li>
                     )
